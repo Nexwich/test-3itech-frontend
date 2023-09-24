e06f3a45a0afcc44c56158e66e1a37e0a6bbbd22
Nexwich <alterfall@mail.ru>
2023-09-24 19:43:46 +0300

    Добавлен файл README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+1. `dist/index.html` - верстка  
+   `src/scss/components/_dialog.scss` - файл стилей
+2. `src/js/modules/decode.js` - функция раскодирования  
--- src/js/modules/decode.js
+++ src/js/modules/decode.js
@@ -6,11 +6,12 @@
  * @returns {string}
  */
 export default input => input
-  .match(/\d+/g).map((x, i) => Math.abs((2 - x) / 2))
+  .match(/\d+/g)
+  .map((x) => Math.abs((2 - x) / 2))
   .join('')
   .split('0.5')
-  .map((x, i, a) => i % 2 ? false : [x, a[i + 1]])
+  .map((x, i, a) => i % 2 ? false : [parseInt(x, 10), parseInt(a[i + 1], 10)])
   .filter((x) => x)
-  .sort((a, b) => parseInt(a[1], 10) > parseInt(b[1], 10))
+  .sort((a, b) => a[1] > b[1])
   .map((x) => String.fromCharCode(x[0]))
   .join('');
